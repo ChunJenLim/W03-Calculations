648ef9d4d7d712d7c05b6010e821c8c04f2a12bc
ChunJenLim <104173618@student.swin.edu.au>
2024-08-22 19:16:56 +1000

    Update on Calculator App (Life Skills)

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var calculationResult: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -22,16 +26,66 @@ class MainActivity : AppCompatActivity() {
 
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
-
         val equals = findViewById<Button>(R.id.equals)
+        val answer = findViewById<TextView>(R.id.answer)
+
         equals.setOnClickListener {
-            val result = add(number1.text.toString(), number2.text.toString())
-            // TODO: show result on the screen
+            calculationResult = add(number1.text.toString(), number2.text.toString())
+            answer.text = calculationResult
+        }
+
+        if (savedInstanceState != null) {
+            calculationResult = savedInstanceState.getString("calculationResult")
+            answer.text = calculationResult
         }
     }
 
-    // adds two numbers together
-    private fun add(number1: String, number2: String) = number1 + number2
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("MainActivity", "onSaveInstanceState")
+        outState.putString("calculationResult", calculationResult)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("MainActivity", "onRestoreInstanceState")
+        calculationResult = savedInstanceState.getString("calculationResult")
+        findViewById<TextView>(R.id.answer).text = calculationResult
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart")
+    }
+
+    private fun add(number1: String, number2: String): String {
+        val num1 = number1.toIntOrNull() ?: 0
+        val num2 = number2.toIntOrNull() ?: 0
+        return (num1 + num2).toString()
+    }
 }
